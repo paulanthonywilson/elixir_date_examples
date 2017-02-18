8eef11bab05488cd9d1d8c19edf93cdbb2011dc1
Paul Wilson <paulanthonywilson@gmail.com>
2017-02-18 23:12:07 +0000

    gregorian days, not day

--- lib/dates.ex
+++ lib/dates.ex
@@ -16,26 +16,26 @@ defmodule Dates do
   """
   @spec weekdays_between(Calendar.date, Calendar.date) :: [Calendar.date]
   def weekdays_between(date1, date2) do
-    days_between(date1, date2, &gregorian_weekday/1)
+    days_between(date1, date2, &gregorian_weekday?/1)
   end
 
   defp days_between(date1, date2, filter) do
-    for g <- (elixir_to_gregorian_date(date1)..elixir_to_gregorian_date(date2)), filter.(g), do:  gregorian_day_to_elixir_date(g)
+    for g <- (elixir_to_gregorian_days(date1)..elixir_to_gregorian_days(date2)), filter.(g), do:  gregorian_days_to_elixir_date(g)
   end
 
-  defp elixir_to_gregorian_date(elixir_date) do
+  defp elixir_to_gregorian_days(elixir_date) do
     elixir_date
     |> Date.to_erl
     |> :calendar.date_to_gregorian_days
   end
 
-  defp gregorian_day_to_elixir_date(gregorian_day) do
+  defp gregorian_days_to_elixir_date(gregorian_day) do
     gregorian_day
     |> :calendar.gregorian_days_to_date
     |> Date.from_erl!
   end
 
-  defp gregorian_weekday(gregorian_day) do
+  defp gregorian_weekday?(gregorian_day) do
     gregorian_day
     |> :calendar.gregorian_days_to_date
     |> :calendar.day_of_the_week
