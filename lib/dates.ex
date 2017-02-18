f46d2d186d4a225e5ff0e3980edc99c65e0286fe
Paul Wilson <paulanthonywilson@gmail.com>
2017-02-18 16:41:54 +0000

    new adventure

---
+++ lib/dates.ex
@@ -0,0 +1,18 @@
+defmodule Dates do
+  @moduledoc """
+  Documentation for Dates.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dates.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dates_test.exs
@@ -0,0 +1,8 @@
+defmodule DatesTest do
+  use ExUnit.Case
+  doctest Dates
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
